9bcf375972416e36da75d6f0d32655dfcd62acb5
Morning <morning@admins-iMac-3.local>
2023-04-06 12:50:01 +0800

    req

--- express-tyoescript/controller/MovieRouter.ts
+++ express-tyoescript/controller/MovieRouter.ts
@@ -6,13 +6,13 @@ const MovieRouter = express.Router();
 MovieRouter.get("/movies/:id", async (req: Request, res: Response) => {
   const result = await MovieModel.find({
     _id: req.params.id,
-  }).limit(1);
+  }).limit(10);
   return res.status(200).send(result);
 });
 
 MovieRouter.get("/movie", async (req: Request, res: Response) => {
   console.log("request hha movie");
-  const result = await MovieModel.find({ poster: { $exists: true } }).limit(4);
+  const result = await MovieModel.find({ poster: { $exists: true } }).limit(10);
 
   res.status(200).send(result);
 });
